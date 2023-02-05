3c1073ca40ef9a25780cc295fae73f607e04f4db
Pavel Valena <pvalena@redhat.com>
2023-02-05 21:54:42 +0100

    2019 / 4

---
+++ 2019/adventofcode4.rb
@@ -0,0 +1,181 @@
+#!/usr/bin/env -S ruby
+
+#DEB = false
+
+require_relative 'class'
+
+class R
+
+  def initialize
+
+#    @s = arg.to_i
+
+    a = {}
+    n = nil
+    s = !false
+    g = !false
+    t = !false
+
+    a = \
+    inp(s: s, g: g, t: t) {
+      |x, i|
+
+      #err :inp, x
+
+      unless true
+
+        xs = x.map { |z| z.to_sym }
+        xi = x.map { |z| z.to_i }
+
+        k = xs.shift
+
+        v = xs.size == 1 ? xi[1] : xs
+
+        a[ k ] = v
+
+        #err :a, a, k, xs
+
+        case s ? x : xs[0]
+          when :Blueprint
+            n = xi[1]
+            a[n] = {}
+
+            c = 2
+
+            #err :xs, xs
+
+            next unless xs[1+c]
+
+          when :Each
+
+            c = 0
+
+            #a[ x[1] ] = [ x[4].split(?=)[1].to_i, x[9..] ]
+
+          else
+            err :inp, i, x, @s
+
+        end
+
+        t = xs[1+c]
+
+        a[n][t] ||= []
+        a[n][t] += [
+            [ xs[5+c], xi[4+c] ],
+            [ xs[8+c], xi[7+c] ]
+          ]
+
+        a[n][t] -= [[nil,nil]]
+
+        #dss :an, a[n]
+      end
+
+      #eval x
+      #x.to_i
+      #to_sym(x)
+      x.split('-').map(&:to_i)
+    }
+
+    ben {
+      @r = run a #.freeze
+    }
+  end
+
+  def run a
+
+    a = a.first
+
+    #err :a, a
+
+    n = 0
+
+    a[0].upto(a[1]).each {
+      |b, i|
+
+
+      n += 1 if fit b
+
+    }
+
+    n
+  end
+
+  def fit x
+
+    z = to_i x.to_s.split('')
+
+    ass :zs, z.size, z.size == 6
+
+    s = false
+    i = true
+
+    r = z[0]
+    z[1..].each {
+      |q|
+    
+      s = true if r == q
+      i = false if r > q
+
+      r = q
+    }
+
+    #err :z, z, s, i
+
+    s && i
+
+  end
+
+  def out c, z, m, **k
+
+    return unless DEB || k[:o]
+
+    w, g = [], []
+
+    c.keys.each {
+      |x|
+
+      v = c[x]
+
+      if v
+
+        w
+
+      else
+
+        g
+
+      end << x
+    }
+
+    q = z.keys + [m]
+
+    super(w, g, q, **k) {
+      |x|
+
+      v = z[x]
+
+      if v
+
+        v = \
+        if v.size == 1
+
+          rrt v[0]
+
+        else
+
+          v.size
+
+        end
+
+        v.to_s.colorize(:green)
+  
+      else
+
+        '@'.colorize(:red)
+
+      end
+    }
+  end
+end
+
+res
