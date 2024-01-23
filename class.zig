8f79251eb9be3261108e6d02cda7d2551ef386e2
Pavel Valena <pvalena@redhat.com>
2024-01-24 00:44:25 +0100

    Enhance class.zig

--- class.zig
+++ class.zig
@@ -2,9 +2,9 @@
 // Const
 pub const std = @import("std");
 
-pub const split = std.mem.split;
 pub const fmt = std.fmt;
 pub const io = std.io;
+pub const mem = std.mem;
 pub const debug = std.debug;
 pub const assert = debug.assert;
 pub const stdout = io.getStdOut().writer();
@@ -179,4 +179,122 @@ pub fn out2(comptime l: anytype, d: anytype, s: anytype, c: anytype) void {
 
 pub fn pr(comptime f: anytype, s: anytype) void {
     stdout.print(f, s) catch {};
+}
+
+pub fn gen(comptime T: anytype) type {
+    const N = Array(T);
+
+    return struct {
+//        var al: G.allocator();
+
+        al: mem.Allocator,
+        d: Array(N),
+        l: *N,
+
+        pub fn init(al: anytype) @This() {
+            var s: @This() = undefined;
+
+            s.al = al;
+            s.d = Array(N).init(al);
+          
+            return s;
+        }
+
+        pub fn items(sl: anytype) []N {
+            return sl.d.items;
+        }
+
+        pub fn h(sl: anytype) usize {
+            return sl.d.items.len;
+        }
+
+        pub fn w(sl: anytype) usize {
+            return sl.l.items.len;
+        }
+
+        pub fn new(sl: anytype) !void {
+            const l = N.init(sl.al);
+
+            try sl.d.append(l);
+
+            const i = sl.d.items;
+
+            sl.l = &i[i.len-1];
+        }
+
+        pub fn add(sl: anytype, v: anytype) !void {
+            try sl.l.append(v);
+        }
+
+        pub fn get(sl: anytype, ii: anytype, cc: anytype) T {
+
+            const i: usize = @intCast(ii);
+            const c: usize = @intCast(cc);
+
+            return sl.d.items[i].items[c];
+        }
+
+        pub fn set(
+            sl: anytype, ii: anytype, jj: anytype,
+            t: anytype
+        ) !void {
+
+            const i: usize = @intCast(ii);
+            const j: usize = @intCast(jj);
+
+            const c = sl.d.items;
+        
+            assert(i < c.len);
+
+            const r = c[i].items;
+
+            assert(j < r.len);
+
+            const x = r[j];
+
+            _ = switch(t) {
+                T.set => assert(x == T.fre),
+                T.fre => assert(x == T.set),
+
+                else => {
+                    try err("set", t);
+                }
+            };
+
+            r[j] = t;
+        }
+
+        pub fn deinit(sl: anytype) void {
+            for (sl.d.items) |i| {
+                i.deinit();
+
+            }
+            sl.d.deinit();
+        }
+    };
+}
+
+pub fn num(v: anytype) !u64 {
+    var n: u64 = undefined;
+
+    if (v[0] >= '0' and v[0] <= '9') {
+        n = fmt.parseUnsigned(u64, v, 10) catch |e| {
+            err("Invalid int", v) catch {};
+
+            return e;
+        };
+    } else {
+        try err("Invalid number", v);
+
+    }
+
+    return n;
+}
+
+pub fn split(d: anytype, s: anytype) mem.SplitIterator(u8,.sequence) {
+    return std.mem.split(u8, d, s);
+}
+
+pub fn eql(d: anytype, s: anytype) bool {
+    return std.mem.eql(u8, d, s);
 }
