1fcd82252246c808e6861145136fc3a7e87f128e
Pavel Valena <pvalena@redhat.com>
2024-05-09 20:27:39 +0200

    WIP: 2023 / 18-2

--- 2023/adventofcode18-2.zig
+++ 2023/adventofcode18-2.zig
@@ -15,6 +15,8 @@ const tst = R.tst;
 const spl = R.spl;
 const eql = R.eql;
 const swp = R.swp;
+const sort = R.sort;
+const sort2 = R.sort2;
 
 const ini = R.ini;
 const res = R.res;
@@ -38,12 +40,15 @@ const HashMap = R.HashMap;
 const HashArray = R.HashArray;
 
 const gen = R.gen;
+const genH = R.genH;
 const num = R.num;
+const chc = R.chc;
 
 
 // Globals //
 const dbg = &R.dbg;
 const aoc = "18";
+
 
 // Input //
 const D = enum {
@@ -52,7 +57,6 @@ const D = enum {
     D,
     L
 };
-const C = [3]u8;
 fn dec(v: anytype) !D {
     return switch(v) {
         'U' => D.U,
@@ -65,7 +69,6 @@ fn dec(v: anytype) !D {
 const I = struct {
     d: D,
     n: u64,
-    c: C
 };
 
 
@@ -80,15 +83,13 @@ fn dis(t: anytype) u8 {
         T.set => '#',
     };
 }
-//const O = struct {
-//    p: [2]u64,
-//    t: T,
-//};
+const K = [2]u64;
 
 
 // Data //
 const A = gen(I);
-const B = gen(T);
+const B = genH(K);
+const C = genH(K);
 
 
 // Logic //
@@ -130,13 +131,12 @@ fn par(al: anytype, d: []u8) !A {
 
             _ = switch(i) {
                 0 => {
-                    if (w.len > 1) try err("par: len", w);
-                    z.d = try dec(w[0]);
+                    continue;
                 },
                 1 =>
-                    z.n = try num(w, u64),
+                    continue,
                 2 => 
-                    z.c = try dat(w),
+                    try dat(w, &z),
                 else => 
                     try err("par: swi", l)
             };
@@ -151,256 +151,386 @@ fn par(al: anytype, d: []u8) !A {
     return a;
 }
 
-fn dat(v: anytype) !C {
+fn dat(v: anytype, z: anytype) !void {
 
-    var c: C = undefined;
+    const i = 2;
+    const s = v[i..i+5];
 
-    var i: usize = 2;
-    var n: usize = 0;
+    z.n = fmt.parseUnsigned(u64, s, 16) catch |e| {
+        puts("s", s);
+        err("dat: hex", s) catch {};
 
-    while (n < 3) {
-        defer n += 1;
-        defer i += 2;
+        return e;
+    };
 
-        const s = v[i..i+2];
+    const j = 7;
+    const t = v[j];
 
-        c[n] = fmt.parseUnsigned(u8, s, 16) catch |e| {
-            err("Invalid hex", v) catch {};
+    z.d = switch(t) {
+        '3' => D.U,
+        '0' => D.R,
+        '1' => D.D,
+        '2' => D.L,
+        else => {
+            try err("dat: swi", t);
+            return error.type;
+        }
+    };
 
-            return e;
-        };
-    }
-
-//    deb("dat", c);
-//    try err("NYI", c);
-
-    return c;
+//    deb("dat", z);
+//    try wip();
 }
 
 
 fn run(al: anytype, d: anytype, z: anytype) !u64 {
 
+    if (z > 0) {
+        dbg.* = true;
+    }
+
     // Unused //
-//    _ = z;
 //    _ = d;
 //    _ = al;
 
     // Timestamp //
 //    const t = tst();
 
+    // Result //
     var b = B.init(al);
     try b.new();
     defer b.deinit();
 
+    var c = C.init(al);
+    try c.new();
+    defer c.deinit();
+
+    var h = HashMap(u64, void).init(al);
+    defer h.deinit();
+
     // Position //
-    const o = 200;
-    var p: [2]u64 = .{ o, o };
+    const o = 10000000;
+    var p: K = .{ o, o };
 
-    // Initial trench //
-    try b.set2(p, T.set);
+    // Initial dig //
+//    try b.putNoClobber(p, {});
+    try b.set2(.{o, p});
 
-    // Input - first array //
+    // Input - first array only //
     const x = d.l;
 
-    for (x.items, 0..) |l, i| {
-//        _ = i;
-
-        if (z == i and z > 0) {
-            dbg.* = true;
-            deb("run", l);
-            pra(b);
-        }
-
-        try ply(&l, &p, &b);
-
-        if (z == i and z > 0) {
-            pra(b);
-            return 0;
-        }
+    for (x.items) |l| {
+        try pla(&l, &p, &b, &h);
     }
 
-    pra(b);
-    try fil(&b);
+//    pra(b);
 
-    return fin(&b);
+    deb("b", b.items().len);
+
+    // Part 2 //
+    try fil(al, &b, &c, &h);
+
+    // Evaluate //
+    return fin(&b, &c);
 //    return 0;
 }
 
-fn ply(
+fn pla(
     l: anytype,
     p: anytype,
-    o: anytype,
+    b: anytype,
+    h: anytype,
 ) !void {
 
-//    deb("ply", .{p, l.d, l.n});
+//    deb("pla", .{p, l.d, l.n});
+//    if (l.n > 0) return;
+
+//    var i = p[0];
+//    var j = p[1];    
+
+    if (l.d == D.R or l.d == D.L) {
+
+//        n = p[1] + (l.n * o);
+//        z = .{ p[0], .{ p[1] + o, n }};
+
+        if (l.d == D.L and p[1] < l.n) {
+            try err("pla: L", .{p.*, l.n});
+        }
+
+        var z = switch (l.d) {
+
+            D.R => .{ p[0], .{ p[1] + 1, p[1] + l.n }},
+            D.L => .{ p[0], .{ p[1] - 1, p[1] - l.n }},
+
+            else => unreachable
+        };
+
+        // Next position //
+        p[1] = z[1][1];
+
+        // Ordered coords //
+        if (z[1][1] < z[1][0]) swp(&z[1][1], &z[1][0]);
+
+        try h.put(p[0], {});
+
+//        deb("z", z);
+        
+        try b.set2(z);
+        return;
+    }
 
     for (0..l.n) |_| {
-
         _ = switch(l.d) {
-            D.R =>
-                p[1] += 1,
-
-            D.L => {
-                assert(p[1] > 0);
-                p[1] -= 1;
-            },
-
             D.D =>
                 p[0] += 1,
 
             D.U => {
-                assert(p[0] > 0);
+                if (p[0] <= 0) {
+                    try err("pla: U", .{p.*, l.n});
+                }
                 p[0] -= 1;
-            }
+            },
+
+            else => unreachable
         };
 
-//        deb("set", p);
+        const z = .{p[0], .{p[1], p[1]}};
 
-        try o.set2(p, T.set);
+        try b.set2(z);
     }
 }
 
-fn fil(bb: anytype) !void {
+// Innards //
+fn fil(
+    al: anytype,
+    b: anytype,
+    c: anytype,
+    h: anytype,
+) !void {
 
-    const b = bb.items();
+//    var n: u64 = 0;
 
-    // left //
-    for (b, 0..) |ll, i| {
+    // pad //
+    try b.new();
 
-        const l = ll.items;
+    for (b.items(), 0..) |ll, i| {
 
-        const j = chcL(l);
+        const lc = ll.count();
 
-        if (j > 0) {
-            filR(bb, i, j);
-            break;
+        if (lc <= 0) continue;
+
+        var l = ll.keys();
+
+        sort2(&l);
+
+        try chcL(l, i, c);
+    }
+
+//    deb("f", try fin(b, c) );
+
+    deb("c", c.items().len);
+
+    const t = tst();
+
+    for (c.items(), 0..) |ll, x| {
+        if (ll.count() <= 0) continue;
+
+        const l = ll.keys();
+
+        for (l) |r| {
+
+            // Fill horizontal lines //
+            if (x > 0) {
+                if (chc(h, x-1)) try filR(al, b, c, x-1, r);
+            }
+            if (chc(h, x+1)) try filR(al, b, c, x+1, r);
+        }
+
+        if (x % 1000000 == 0) {
+            deb("x", .{x, tst() - t});
         }
     }
 
-    pra(bb);
+    deb("c", c.items().len);
 }
 
-fn filR(bb: anytype, i: anytype, j: anytype) void {
+fn chcL(l: anytype, x: anytype, c: anytype) !void {
 
-    const b = bb.items();
+    var z: u64 = 0;
+    var i = l[z];
 
-    if (i < 1 or i >= b.len) return;
+    const ll = l.len - 1;
 
-    var l = b[i].items;
+    var s = true;
 
-    if (j < 1 or j >= l.len or l[j] != T.fre) return;
+    // Loop //
+    while (z < ll) {
+        z += 1;
 
-    l[j] = T.set;
+        if(i[0] != i[1]) return;
 
-    filR(bb, i, j+1);
-    filR(bb, i, j-1);
+        const ii = i[0];
 
-    filR(bb, i+1, j);
-    filR(bb, i-1, j);
+        // Next = empty //
+        if (try wal(l, ii + 1)) return;
+
+        const j = l[z];
+        const jj = j[0];
+
+        assert(j[0] <= j[1]);
+        
+        if (s) {   
+            _ = try setx(c, x, ii, jj);
+        }
+
+        i = j;
+        s = !s;
+    }
 }
 
-fn chcL(l: anytype) usize {
+fn filR(al: anytype, b: anytype, c: anytype, x: anytype, g: anytype) !void {
 
-    var i: usize = 0;
+//    _ = al;
 
-    if (l.len <= 0) return 0;
+    var s: u64 = 0;
 
-    while (l[i] == T.fre) {
-        i += 1;
+    assert(x < b.items().len);
 
-        if (i >= l.len) return 0;
+    var l = b.items()[x].keys();
+    const ll = l.len - 1;
+
+    sort2(&l);
+
+    var k: u64 = 0;
+
+    for (g[0]+1..g[1]) |z| {
+
+        if (s > z) continue;
+
+        // Don't hit the wall //
+        if (!try wal(l, z)) {
+
+            // Look up borders //
+            while (k < ll) {
+
+                const i = l[k][1];
+                const j = l[k+1][0];
+
+                assert(i < j);
+            
+                if (i < z and z < j) {
+
+                    const y = try setx(c, x, i, j);
+
+                    if (y) {
+                        const r = .{i, j};
+                    
+                        deb("filR", .{x, r});
+
+                        // rec //
+                        if (x > 0) try filR(al, b, c, x-1, r);
+                        try filR(al, b, c, x+1, r);
+                    }
+                    
+                    s = j;
+                    break;
+                }
+
+                k += 1;
+            }
+
+            if (k >= ll) return;
+        }
     }
-    if (l[i] != T.set) return 0;
-
-    while (l[i] == T.set) {
-        i += 1;
-
-        if (i >= l.len) return 0;
-    }
-    if (l[i] != T.fre) return 0;
-
-    const j = i;
-
-    while (l[i] == T.fre) {
-
-        i += 1;
-
-        if (i >= l.len) return 0;
-    }
-    if (l[i] != T.set) return 0;
-
-    return j;
 }
 
-//fn mov(
-//    x: anytype, l: anytype, v: anytype, n: anytype,
-//    h: anytype, r: anytype
+fn wal(l: anytype, z: anytype) !bool {
+
+//    CHC:
+//            const v = ll.get(cc);
 //
-//) !void {
+//            if (v) |_| {
+//                return true;
+//            }
 //
-//    const d = (l.d[0] == h and l.d[1] == r);
-//    
-//    if (d and l.m >= 3) return;
+
+    for (l) |k| {
+        const i = k[0];
+        const j = k[1];
+
+        if (i <= z and z <= j) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn setx(c: anytype, x: anytype, i: anytype, j: anytype) !bool {
+
+    assert(i < j);
+
+    const r = .{i, j};
+
+    if (!c.chc(x, r)) {
+        try c.set2(.{x, r});
+
+        return true;
+    }
+
+    return false;
+
+//    var f = HashMap(K, void).init(al);
+//    defer f.deinit();
 //
-//    const p: K = .{ l.p[0] + h, l.p[1] + r };
+//    var n = HashMap(K, void).init(al);
+//    defer n.deinit();
 //
-//    if (p[0] < 0 or p[1] < 0) return;
-//    if (p[0] >= x.h() or p[1] >= x.w()) return;
+//    try f.put(p, {});
+//         
+//    while (f.count() > 0) {
 //
-//    var z: L = undefined;
-//    z.p = p;
-//    z.d = .{h, r};
-//    z.m = if (d) l.m + 1 else 1;
+//        n.clearRetainingCapacity();
 //
-//    var w: V = undefined;
+//        var q = f.keyIterator();
 //
-//    w.v = x.get2(p) + v.v;
+//        while (q.next()) |k| {
 //
-//    w.p = try v.p.clone();
+//            const i = k[0];
+//            const j = k[1];
 //
-//    try w.p.append(z.p);
+//            // EnQueue! //
+//            try bchc(b, &n, i, j+1);
+//            try bchc(b, &n, i, j-1);
 //
-////    try err("mov", w.v);
-//
-//    // go //
-//    _ = try lput(n, z, w);
-//}
-//
-//fn lput(s: anytype, k: anytype, v: anytype) !bool {
-//    const g = s.*.get(k);
-//
-//    if (g) |c| {
-//        if (v.v < c.v) {
-//            // Current bad //
-//            c.p.deinit();
-//            try s.*.put(k, v);
-//   
-//        } else {
-//            // Current good //
-//            v.p.deinit();
-//            return false;
-// 
+//            try bchc(b, &n, i+1, j);
+//            try bchc(b, &n, i-1, j);
 //        }
 //
-//    } else {
-//        // New entry //
-//        try s.*.putNoClobber(k, v);
-//
+//        swp(&f, &n);
 //    }
-//
-//    return true;
-//}
+}
 
-fn fin(b: anytype) !u64 {
+fn fin(b: anytype, c: anytype) !u64 {
 
     var n: u64 = 0;
 
+    // Borders //
     for (b.items()) |l| {
 
-        for (l.items) |v| {
-            if (v == T.set) n += 1;
-        }
+        for (l.keys()) |k| {
 
+            n += k[1] - k[0] + 1;
+        }
+    }
+    
+    // Fill //
+    for (c.items()) |l| {
+
+        for (l.keys()) |k| {
+
+            n += k[1] - k[0] - 1;
+
+        }
     }
     
     return n;
@@ -453,12 +583,13 @@ fn pra(a: anytype) void {
 
 
 // Main //
-fn tmain(al: anytype, nn: anytype) !void {
+fn rmain(al: anytype, nn: anytype) !void {
     puts("", "");
 
     var n: u64 = nn;
 
     const f = "data" ++ aoc ++ "-t.txt";
+//    const f = "data" ++ aoc ++ ".txt";
 
     const d = try ini(al, f, &n);
     defer al.free(d);
@@ -479,16 +610,16 @@ fn tmain(al: anytype, nn: anytype) !void {
 pub fn main() !void {
 
     // Fun! //
-    var are = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer are.deinit();
-    const al = are.allocator();
+//    var are = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+//    defer are.deinit();
+//    const al = are.allocator();
 
     // Speed! //
-//    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-//    defer _ = gpa.deinit();
-//    const al = gpa.allocator();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const al = gpa.allocator();
 
-    try tmain(al, 0);
+    try rmain(al, 0);
 }
 
 
@@ -500,5 +631,5 @@ test "main" {
 
     const al = std.testing.allocator;
 
-    try tmain(al, 0);
+    try rmain(al, 0);
 }
