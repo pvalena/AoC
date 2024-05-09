1b97101247b38a181749026777de6d21a5297847
Pavel Valena <pvalena@redhat.com>
2024-05-09 20:27:20 +0200

    Enhance zigrun.sh

--- 2023/zigrun.sh
+++ 2023/zigrun.sh
@@ -17,12 +17,51 @@ shift ||:
   :
 } || X=
 
-a='-freference-trace'
-#a="${a} -OReleaseFast"
+[[ "$1" == '-v' ]] && {
+  V="$2"
+  shift 2 ||:
+  :
+} || V=
 
-f="adventofcode${n}${p:+-$p}.zig"
+###
+
+a='-freference-trace'
+aa="-OReleaseFast"
+
+s="${n}${p:+-$p}"
+
+f="adventofcode${s}.zig"
+l="out${s}.log"
 
 t=30
 
-exec cst -c "$f" \
-  "set -x ; echo ; echo; clear; zig test $a $f && timeout ${t} zig run $a $f -- data${n}.txt $X"
+###
+
+con () {
+  read "?--> Continue?" ||:
+}
+
+###
+
+set +x
+
+while :; do
+  cst -c "$f" "echo ; echo; clear; set -x ; timeout ${t} time zig test $a $f && exit" | tee "$l"
+
+  [[ -n "$V" ]] && {
+    echo
+
+    grep -E "^\s*=> ${V}$" "$l" || {
+      con
+      continue
+    }
+  }
+
+  echo
+  set -x
+
+  time zig run $aa $f -- "data${n}.txt" $X || echo FAIL
+
+  { set +x ; } &>/dev/null
+  con
+done
